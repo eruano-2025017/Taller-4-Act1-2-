a892573fa9a763965fcdb2eb8eff6d5016788da5
eruano-2025017 <estebanguatemala33@gmail.com>
2026-09-02 11:53:24 -0600

    build: se modifica archivo front/src/app/services/dashboard.service.ts

--- front/src/app/services/dashboard.service.ts
+++ front/src/app/services/dashboard.service.ts
@@ -1,15 +1,25 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { environment } from "../../environments/environment";
-import { DashboardData } from "../shared/models/dashboard.model";
+import { DashboardData, YearChartResponse } from "../shared/models/dashboard.model";
 
 @Injectable({ providedIn: "root" })
 export class DashboardService {
   constructor(private http: HttpClient) {}
 
-  getDashboardData(): Observable<DashboardData> {
-    return this.http.get<DashboardData>(`${environment.apiUrl}/dashboard`);
+  getDashboardData(year?: number): Observable<DashboardData> {
+    let params = new HttpParams();
+    if (year && !isNaN(year)) {
+      params = params.set("year", year.toString());
+    }
+    return this.http.get<DashboardData>(`${environment.apiUrl}/dashboard`, { params });
+  }
+
+  getYearChart(year: number): Observable<YearChartResponse> {
+    return this.http.get<YearChartResponse>(`${environment.apiUrl}/dashboard/chart`, {
+      params: { year: year.toString() }
+    });
   }
 }
 
